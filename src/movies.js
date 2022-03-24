205283fb8525b37f7222ab3ced888109d9f8d71c
William Berbet <wiiliamb@hotmail.com>
2022-03-24 17:43:11 -0300

    lab 24.03, fiz o que consegui

--- src/movies.js
+++ src/movies.js
@@ -1,3 +1,5 @@
+const movies = require("./data")
+
 // The `movies` array from the file `src/data.js`.
 console.log('movies: ', movies);
 
@@ -5,16 +7,51 @@ console.log('movies: ', movies);
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(arr) {
+    const todosDiretores = arr.map((currentElement) => {
+    return currentElement.director
+  })
+  console.log(todosDiretores);
+  return todosDiretores;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(arr) {
+
+  const spielbergMovies = arr.filter(function(obj) {
+    if (obj.diretor === 'Steven Spielberg' && obj.genre.includes('Drama')) {
+      return true;
+    }
+    
+  })
+  return spielbergMovies.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+    const reduces = movies.reduce(function(acumulador, elemento) {
+      return acumulador + elemento.score
+    }, 0);
+    console.log(( reduces / movies.length).toFixed(2))
+    return parseFloat((reduces / movies.length).toFixed(2));
+    
+}
+scoresAverage(movies);
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  const dramaMovies = movies.filter(function(obj) {
+    if (obj.genre === 'Drama'){
+      return true; 
+    }
+  
+  });
+  const totalScore = dramaMovies.reduce(function(acumulador, elemento) {
+    return acumulador + elemento.score
+  }, 0);
+
+  return parseFloat((totalScore / dramaMovies.length).toFixed(2));
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
